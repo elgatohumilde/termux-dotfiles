83b7fa6a447d25c96df8b4584201a6353e45bf1e
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-03-16 17:28:13 -0500

    change install script

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ set -o errexit -o pipefail -o nounset
 termux-change-repo
 termux-setup-storage
 termux-setup-package-manager
+pkg install root-repo x11-repo
 pkg upgrade
-pkg install android-tools tmux termux-api fzf fd ripgrep neovim vim-gtk gitui openjdk-17
+pkg install android-tools tmux termux-api fzf fd ripgrep neovim vim-gtk gitui openjdk-17 tree ranger
